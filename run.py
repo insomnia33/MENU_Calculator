b0c94be8a1ffeb346918e862208690f36e222679
insomnia33 <pcesar333@gmail.com>
2021-11-29 19:19:16 -0300

    update

--- run.py
+++ run.py
@@ -4,8 +4,30 @@ import numpy                as np
 import plotly.express       as px
 from datetime               import timedelta
 import SessionState
+import base64
 
 elements = {'Tc99m':361.2,'I131':11520.0,'I123':780.0,'Ga67':4698.0,'Ga68':68110.0,'Tl201':4380.0,'F18':109.8}
+
+def download_link(object_to_download, download_filename, download_link_text):
+    """
+    Generates a link to download the given object_to_download.
+
+    object_to_download (str, pd.DataFrame):  The object to be downloaded.
+    download_filename (str): filename and extension of file. e.g. mydata.csv, some_txt_output.txt
+    download_link_text (str): Text to display for download link.
+
+    Examples:
+    download_link(YOUR_DF, 'YOUR_DF.csv', 'Click here to download data!')
+    download_link(YOUR_STRING, 'YOUR_STRING.txt', 'Click here to download your text!')
+
+    """
+    if isinstance(object_to_download,pd.DataFrame):
+        object_to_download = object_to_download.to_csv(index=False)
+
+    # some strings <-> bytes conversions necessary here
+    b64 = base64.b64encode(object_to_download.encode()).decode()
+
+    return f'<a href="data:file/txt,base64,{b64}" download="{download_filename}">{download_link_text}</a>'
 
 def hourToMin(hour1, hour2):
     hour1 = hour1.strftime("%H:%M")
@@ -41,11 +63,13 @@ state = SessionState.get(fixos={'meiavida': '','qtd0': 0,'hora0': 0},entrada = {
 
 
 
-tit, open1, save1, clear1 = st.beta_columns(4)
+tit, buttons, results = st.beta_columns(3)
 tit.title("MENU Calculator")
-openFile = open1.button('Abrir')
-saveFile = save1.button('Salvar')
-clearFile = clear1.button('Apagar dados')
+openFile = buttons.button('Abrir')
+upload = results.empty()
+saveFile = buttons.button('Salvar')
+download = results.empty()
+clearFile = buttons.button('Apagar dados')
 
 
 my_expander = st.beta_expander("Entradas:", expanded=True)
@@ -78,10 +102,6 @@ if clearFile:
         'Hora da Aplicação' : []
     }
 
-
-my_expander = st.beta_expander("Dados:", expanded=True)
-table = my_expander.table(state.entrada)
-
 if calc:
     # state.fixos['meiavida'] = elements[element]
     # state.fixos['qtd0'] = qtd0
@@ -90,8 +110,22 @@ if calc:
     horas, sobras = calculate(state.entrada, elements[element], qtd0, hora0)
     state.entrada['Sobra (mCi)'] = sobras
 
-
 if graph:
       
     fig = px.line(state.entrada, x="Hora da Aplicação", y="Sobra (mCi)", title='Sobra por horario de aplicação')  
     st.plotly_chart(fig)
+
+if openFile:
+    upload.file_uploader('', type=['csv', 'xlxs'])
+
+if saveFile:
+    df = pd.DataFrame.from_dict(state.entrada)
+    tmp_download_link = download_link(df, 'menu_data.csv', 'DOWNLOAD LINK')
+    download.markdown(tmp_download_link, unsafe_allow_html=True)
+
+
+
+my_expander = st.beta_expander("Dados:", expanded=True)
+table = my_expander.table(state.entrada)
+
+
